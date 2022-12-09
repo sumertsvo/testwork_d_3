4224cd2c6a21d448baccec484a9b422c245a9657
vlad <vlad.s.o@yandex.ru>
2022-12-09 20:22:53 +0300

    dictionary_add

--- Unit1.pas
+++ Unit1.pas
@@ -15,7 +15,14 @@ type
     DataOut: TMemo;
     Label1: TLabel;
     Label2: TLabel;
+    searchstring: TEdit;
+    Label3: TLabel;
     procedure StartClick(Sender: TObject);
+    procedure searchstringChange(Sender: TObject);
+    procedure searchstringClick(Sender: TObject);
+    procedure searchstringMouseActivate(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y, HitTest: Integer;
+      var MouseActivate: TMouseActivate);
   private
     { Private declarations }
   public
@@ -24,10 +31,38 @@ type
 
 var
   Form1: TForm1;
+  dataPair: tDictionary<string, string>;
 
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.searchstringChange(Sender: TObject);
+var
+  bufstr: string;
+begin
+if not (searchString.Text = '') then
+
+  if dataPair.ContainsKey(searchstring.Text) then
+  begin
+    dataPair.TryGetValue(searchstring.Text, bufstr);
+    Label3.Caption := bufstr;
+  end
+  else
+    Label3.Caption := 'article not found';
+end;
+
+procedure TForm1.searchstringClick(Sender: TObject);
+begin
+  searchstring.Clear;
+end;
+
+procedure TForm1.searchstringMouseActivate(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y, HitTest: Integer;
+  var MouseActivate: TMouseActivate);
+begin
+       searchstring.Clear;
+end;
 
 procedure TForm1.StartClick(Sender: TObject);
 var
@@ -36,21 +71,27 @@ var
 begin
 
   try
-    data := TJSONObject.ParseJSONValue(DataIn.text, true, true)
+    data := TJSONObject.ParseJSONValue(DataIn.Text, true, true)
       .AsType<TJSONObject>;
 
     bufferArray := data.FindValue('result').FindValue('offerMappingEntries')
       .AsType<TJSONArray>;
 
+    dataPair:= TDictionary<string, string>.Create;
+
     with bufferArray.GetEnumerator do
       while moveNext do
       begin
 
+       dataPair.Add(current.FindValue('offer').FindValue('shopSku').ToString,
+          current.FindValue('offer').FindValue('name').ToString);
+
         DataOut.Lines.Add(
 
           'art: ' + current.FindValue('offer').FindValue('shopSku').ToString +
-          ' count: ' + (current.FindValue('offer').FindValue('pictures').AsType<TJSONArray>).Count.ToString +
-           ' status: ' + current.FindValue('offer').FindValue('availability').ToString
+          ' count: ' + (current.FindValue('offer').FindValue('pictures')
+          .AsType<TJSONArray>).Count.ToString + ' status: ' +
+          current.FindValue('offer').FindValue('availability').ToString
 
           );
       end;
